5fcf18d463201fbb08be20211305aaedaccc3906
Sharzil <sharzil362@gmail.com>
2025-11-25 19:40:45 -0600

    made the default timer for the exam date

---
+++ countdown.js
@@ -0,0 +1,22 @@
+const days = document.getElementById("one");
+const hours = document.getElementById("two");
+const minutes = document.getElementById("three");
+const seconds = document.getElementById("four");
+
+const targetDate = new Date("December 16, 2025 09:00:00").getTime();
+
+function timer(){
+    const now = new Date().getTime();
+    const timeremaining = targetDate - now;
+    
+    const d = Math.floor(timeremaining / (1000 * 60 * 60 * 24));
+    const h = Math.floor((timeremaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const m = Math.floor((timeremaining % (1000 * 60 * 60)) / (1000 * 60));
+    const s = Math.floor((timeremaining % (1000 * 60)) / 1000);
+    
+    days.innerHTML = d;
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+}
+setInterval(timer, 1000);
